7f2583681103efcae55ff4d98cc8086ab2f6b3c1
joao.pedro6 <joao.pedro@ubistart.com>
2022-06-21 18:42:00 -0300

    Finish TODO List to Learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {v4 as uuidv4} from "uuid"
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
 
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
@@ -9,61 +10,79 @@ import Header from "./components/Header";
 import './App.css';
 import TaskDetails from "./components/TaskDetails";
 
-const App = () => {
-  const [tasks, setTasks] = useState([{
-    id: '1',
-    title: 'Estudar Programação',
-    completed: false
-  },
-{
-  id:"2",
-  title: "Ler livros",
-  completed: true
-}])
 
-const hanldeTaskAddition = (taskTitle) => {
-  const newTasks = [...tasks, {
-    title: taskTitle,
-    id: uuidv4(),
-    completed: false
-  }]
-  setTasks(newTasks)
+class App extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>
+  }
 }
 
-const handleTaskClick = (taskId) => {
-  const newTasks = tasks.map((task) => {
-    if(task.id === taskId) return {
-      ...task,
-      completed: !task.completed
-    }
+export default App;
+// const App = () => {
+//   const [tasks, setTasks] = useState([{
+//     id: '1',
+//     title: 'Estudar Programação',
+//     completed: false
+//   },
+// {
+//   id:"2",
+//   title: "Ler livros",
+//   completed: true
+// }])
 
-    return task
-  })
-  setTasks(newTasks)
-}
+// useEffect(() => {
+//   handleGetTasks()
+// }, [])
 
-const handleTaskDelete = (taskId) => {
-  const newTasks = tasks.filter((task) => {
-    return task.id !== taskId
-  })
-  setTasks(newTasks)
-}
-  return (
-    <div className="container">
-      <Header/>
-      <Routes>
-        <Route path="/" exact element={
-          <>
-            <AddTask hanldeTaskAddition={hanldeTaskAddition} />
-            <Tasks tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDelete={handleTaskDelete}/>
-          </>
-        }>
-        </Route>
-        <Route path="/:taskTitle" exact element={<TaskDetails/>}>
-        </Route>
-      </Routes>
-    </div>
-  )
-}
+// const handleGetTasks = async () => {
+//   const {data} = await axios.get("https://jsonplaceholder.cypress.io/todos?_limit=10");
+//   setTasks(data);
+// }
+
+// const hanldeTaskAddition = (taskTitle) => {
+//   const newTasks = [...tasks, {
+//     title: taskTitle,
+//     id: uuidv4(),
+//     completed: false
+//   }]
+//   setTasks(newTasks)
+// }
+
+// const handleTaskClick = (taskId) => {
+//   const newTasks = tasks.map((task) => {
+//     if(task.id === taskId) return {
+//       ...task,
+//       completed: !task.completed
+//     }
+
+//     return task
+//   })
+//   setTasks(newTasks)
+// }
+
+// const handleTaskDelete = (taskId) => {
+//   const newTasks = tasks.filter((task) => {
+//     return task.id !== taskId
+//   })
+//   setTasks(newTasks)
+// }
+
+//   return (
+//     <div className="container">
+//       <Header/>
+//       <Routes>
+//         <Route path="/" exact element={
+//           <>
+//             <AddTask hanldeTaskAddition={hanldeTaskAddition} />
+//             <Tasks tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDelete={handleTaskDelete}/>
+//           </>
+//         }>
+//         </Route>
+//         <Route path="/:taskTitle" exact element={<TaskDetails/>}>
+//         </Route>
+//       </Routes>
+//     </div>
+//   )
+// }
  
-export default App
+// export default App
